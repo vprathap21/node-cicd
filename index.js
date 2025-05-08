9a16e5410d856913be9128c59641f18d68560eb3
vprathap21 <vprathap585@gmail.com>
2025-05-08 15:16:01 +0530

    test github action

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js deployed via GitHub Actions!');
+  res.send('Hello from Node.js deployed via GitHub Actions!-----------> prathap');
 });
 
 app.listen(port, () => {
