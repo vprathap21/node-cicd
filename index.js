aee36e7865ba9d4ae23f51023a93016b0527a89f
vprathap21 <vprathap585@gmail.com>
2025-05-08 14:37:03 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js deployed via GitHub Actions!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
